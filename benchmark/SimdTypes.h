efdf8cd5d6e0cad078cce1af8e9e14e20e2587f2
San-Zh <klzhang@cnic.cn>
2023-09-10 15:20:12 +0800

    SimdTypes &  format

--- benchmark/SimdTypes.h
+++ benchmark/SimdTypes.h
@@ -1,53 +1,49 @@
+/**
+ * @file SimdTypes.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-09-10
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 
 #pragma once
+
 #include "Simd_vComplex.h"
 
-template <typename Tp>
-class vRealTraits;
+// clang-format off
+template <typename Tp> class vRealTraits;
+template <> class vRealTraits<float>  { typedef vRealF value_type; };
+template <> class vRealTraits<double> { typedef vRealD value_type; };
 
-template <>
-class vRealTraits<float> {
-  public:
-    typedef vRealF value_type;
-};
-
-template <>
-class vRealTraits<double> {
-  public:
-    typedef vRealD value_type;
-};
-
-template <class vtype>
-class SimdTraits;
-
-template <>
-class SimdTraits<vRealF> {
-    typedef vRealF value_type;
-};
-
-template <>
-class SimdTraits<vRealD> {
-    typedef vComplexD value_type;
-};
-
-template <>
-class SimdTraits<vComplexF> {
-    typedef vComplexF value_type;
-};
-
-template <>
-class SimdTraits<vComplexD> {
-    typedef vComplexD value_type;
-};
+template <class vtype> class SimdTraits;
+template <> class SimdTraits<vRealF>    { typedef vRealF    value_type; };
+template <> class SimdTraits<vRealD>    { typedef vRealD    value_type; };
+template <> class SimdTraits<vComplexF> { typedef vComplexF value_type; };
+template <> class SimdTraits<vComplexD> { typedef vComplexD value_type; };
+// clang-format on
 
 template <class vtype, int N>
 class iVector {
   public:
     typedef typename SimdTraits<vtype>::value_type value_type;
-    value_type                                     Vec[N];
+    value_type Vec[N];
 };
-template <class vtype, int N>
+
+template <class vtype, int M, int N = M>
 class iMatrix {
     typedef typename SimdTraits<vtype>::value_type value_type;
-    value_type                                     Mat[N][N];
+    value_type Mat[M][N];
 };
+
+template <typename vtype, int M, int N>
+auto VVM_Fmadd(iVector<vtype, M> &va, iVector<vtype, N> &vb, iMatrix<vtype, M, N> &m)
+{
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            m.Mat[i][j] = SimdFmadd(a.Vec[i], b.Vec[j], m.Mat[i][j]);
+        }
+    }
+}
