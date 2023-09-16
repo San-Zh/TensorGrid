83def4a7919085f1e6770196500db5ecc5ff0b27
kelong <klzhang@cnic.cn>
2023-09-16 22:10:56 +0800

    iVertor & load

--- include/Simd/SimdTupleTypes.h
+++ include/Simd/SimdTupleTypes.h
@@ -11,17 +11,31 @@
 
 #include "SimdTraits.h"
 
-template <class vtype, int N>
+template <class vtype, unsigned _N>
 class iVector {
   public:
-    typename SimdTraits<vtype>::vtype vec[N];
+    vtype       &operator[](unsigned _n) { return vec[_n]; }
+    const vtype &operator[](unsigned _n) const { return vec[_n]; }
+    enum { Size = _N };
+
+  private:
+    vtype vec[_N];
 };
 
-template <class vtype, int M, int N = M>
-class iMatrix {
-  public:
-    typename SimdTraits<vtype>::vtype mat[M][N];
-};
+// template <class vtype, unsigned _M, unsigned _N = _M>
+// class iMatrix {
+//   public:
+//     typedef typename SimdTraits<vtype>::vtype vtype;
+//     typedef typename SimdTraits<vtype>::dtype dtype;
+
+//   public:
+//     // vtype       &operator[][](unsigned _m, unsigned _n) { return mat[_m][_n]; }
+//     // const vtype &operator[][](unsigned _m, unsigned _n) const { return mat[_m][_n]; }
+//     enum {};
+
+//   private:
+//     vtype _mat[_M][_N];
+// };
 
 // template <class vtype, int M, int N = M>
 // class iMatrix {
@@ -36,3 +50,17 @@ using kerVector = typename SimdTraits<vtype>::vtype[N];
 
 template <class vtype, unsigned M, unsigned N>
 using kerMatrix = typename SimdTraits<vtype>::vtype[M][N];
+
+
+// template <class iVect, unsigned _N>
+// void iVectorLoad(iVect &A, const typename iVect::ptr_type _p)
+// {
+//     for (unsigned i = 0; i < _N; i++) { SimdLoad(A[i], _p[i]); }
+// }
+
+
+template <class vtype, unsigned _N>
+static inline void iVectorLoad(iVector<vtype, _N> &A, typename SimdTraits<vtype>::ptr_type *_p)
+{
+    for (unsigned i = 0; i < _N; i++) { SimdLoad(A[i], _p[i]); }
+}
