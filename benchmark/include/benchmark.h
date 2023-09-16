9e941ff7568d6b9225a0f6b1403205d152b057bb
kelong <klzhang@cnic.cn>
2023-09-16 22:15:08 +0800

    basic gemv, and benchmark v0.0.1 & v0.0.2

--- benchmark/include/benchmark.h
+++ benchmark/include/benchmark.h
@@ -12,6 +12,13 @@
 #pragma once
 
 #include "benchmark_base_generic.h"
+
 #include "benchmark_TensorGridBlas_autosimd.h"
+
+#if defined(__AVX__) || defined(__AVX2__)
 #include "benchmark_TensorGridBlas_avx256.h"
+#endif
+
+#if defined(__AVX512F__)
 #include "benchmark_TensorGridBlas_avx512.h"
+#endif
